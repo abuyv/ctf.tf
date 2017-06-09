44d0b98688fb5c6b799aef546fffa7c34cd1c19d
Eun <eun@su.am>
2017-06-09 17:01:16 +0200

    Improvements

--- create_links.sh
+++ create_links.sh
@@ -1,126 +1,18 @@
+#!/bin/bash
+
+TOOL="echo"
+
+function linkrange {
+    i=1
+    for i in {$2..$3}
+    do
+        echo $1.html $i.html
+    done
+}
+
+
+
 cd _browse
-ln -s 1.html 2.html
-ln -s 1.html 3.html
-ln -s 1.html 4.html
-ln -s 1.html 5.html
-ln -s 1.html 6.html
-ln -s 1.html 7.html
-ln -s 1.html 8.html
-ln -s 1.html 9.html
-ln -s 1.html 10.html
-ln -s 1.html 11.html
-ln -s 1.html 12.html
-ln -s 1.html 13.html
-ln -s 1.html 14.html
-ln -s 1.html 15.html
-ln -s 1.html 16.html
-ln -s 1.html 17.html
-ln -s 1.html 18.html
-ln -s 1.html 19.html
-ln -s 1.html 20.html
-cd ..
+linkrange 1 2 20
 
-cd _easy
-ln -s 1.html 2.html
-ln -s 1.html 3.html
-ln -s 1.html 4.html
-ln -s 1.html 5.html
-ln -s 1.html 6.html
-ln -s 1.html 7.html
-ln -s 1.html 8.html
-ln -s 1.html 9.html
-ln -s 1.html 10.html
-ln -s 1.html 11.html
-ln -s 1.html 12.html
-ln -s 1.html 13.html
-ln -s 1.html 14.html
-ln -s 1.html 15.html
-ln -s 1.html 16.html
-ln -s 1.html 17.html
-ln -s 1.html 18.html
-ln -s 1.html 19.html
-ln -s 1.html 20.html
-cd ..
-
-cd _hard
-ln -s 1.html 2.html
-ln -s 1.html 3.html
-ln -s 1.html 4.html
-ln -s 1.html 5.html
-ln -s 1.html 6.html
-ln -s 1.html 7.html
-ln -s 1.html 8.html
-ln -s 1.html 9.html
-ln -s 1.html 10.html
-ln -s 1.html 11.html
-ln -s 1.html 12.html
-ln -s 1.html 13.html
-ln -s 1.html 14.html
-ln -s 1.html 15.html
-ln -s 1.html 16.html
-ln -s 1.html 17.html
-ln -s 1.html 18.html
-ln -s 1.html 19.html
-ln -s 1.html 20.html
-cd ..
-
-
-cd _latest
-ln -s 1.html 2.html
-ln -s 1.html 3.html
-ln -s 1.html 4.html
-ln -s 1.html 5.html
-ln -s 1.html 6.html
-ln -s 1.html 7.html
-ln -s 1.html 8.html
-ln -s 1.html 9.html
-ln -s 1.html 10.html
-ln -s 1.html 11.html
-ln -s 1.html 12.html
-ln -s 1.html 13.html
-ln -s 1.html 14.html
-ln -s 1.html 15.html
-ln -s 1.html 16.html
-ln -s 1.html 17.html
-ln -s 1.html 18.html
-ln -s 1.html 19.html
-ln -s 1.html 20.html
-cd ..
-
-cd _index
-ln -s 0.html 1.html
-ln -s 0.html 2.html
-ln -s 0.html 3.html
-ln -s 0.html 4.html
-ln -s 0.html 5.html
-ln -s 0.html 6.html
-ln -s 0.html 7.html
-ln -s 0.html 8.html
-ln -s 0.html 9.html
-ln -s 0.html a.html
-ln -s 0.html b.html
-ln -s 0.html c.html
-ln -s 0.html d.html
-ln -s 0.html e.html
-ln -s 0.html f.html
-ln -s 0.html g.html
-ln -s 0.html h.html
-ln -s 0.html i.html
-ln -s 0.html j.html
-ln -s 0.html k.html
-ln -s 0.html l.html
-ln -s 0.html m.html
-ln -s 0.html n.html
-ln -s 0.html o.html
-ln -s 0.html p.html
-ln -s 0.html q.html
-ln -s 0.html r.html
-ln -s 0.html s.html
-ln -s 0.html t.html
-ln -s 0.html u.html
-ln -s 0.html v.html
-ln -s 0.html w.html
-ln -s 0.html x.html
-ln -s 0.html y.html
-ln -s 0.html z.html
 cd ..
